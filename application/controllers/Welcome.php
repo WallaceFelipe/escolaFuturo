bfe7ff608ee55541a067f2deacadc17092744cba
LeticiaGuedes <le_10_lele@hotmail.com>
2016-11-26 14:12:34 -0200

    Atualização CI

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,13 +20,6 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-  // Load URL Helpers
-  $this->load->helper('url');
-
-  // Create a URL
-  $url = base_url() . 'turmas';
-
-  // Redirect user
-  redirect($url);
+		$this->load->view('welcome_message');
 	}
 }
